e3b3701d904d0bd13c76a40dde63a8df22606e2d
daniel-tanaka-x <159022764+daniel-tanaka-x@users.noreply.github.com>
2024-09-13 21:00:05 +0200

    Create run_piggybank.sh
    Added a script to run when raspberry pi gets booted

---
+++ run_piggybank.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Activate the virtual environment
+source /home/pi/venv/bin/activate
+
+# Change directory to the project folder
+cd /home/pi/
+
+# Run the Python script
+python piggybank.py
